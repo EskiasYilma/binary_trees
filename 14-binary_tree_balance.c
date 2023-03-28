48cd2962a60e14802562b6822dc0ed1dc9cd9687
exuszee <exuszee@gmail.com>
2023-03-28 16:58:30 +0300

    fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,10 +13,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
 	l_height = binary_tree_height(tree->left);
 	r_height = binary_tree_height(tree->right);
 
-	return (1 + (l_height > r_height ? l_height : r_height));
+	if (l_height > r_height)
+		return (1 + l_height);
+	else
+		return (1 + r_height);
 }
 
 /**
