dc840136327e2fd6d9dd1404e4ba5f4de1227e28
exuszee <exuszee@gmail.com>
2023-03-28 19:31:06 +0300

    fix

--- binary_trees.h
+++ binary_trees.h
@@ -45,7 +45,6 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 void print_level(const binary_tree_t *tree, int level, void (*func)(int));
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
-size_t binary_tree_height(const binary_tree_t *tree);
 int check_bst(const binary_tree_t *tree);
 int check_bal(const binary_tree_t *tree);
 int binary_tree_is_avl(const binary_tree_t *tree);
